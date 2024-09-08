91f46ddad06e6c99c5ad90df7a56cf04248df9a4
hurenn <suzukihuren070@gmail.com>
2024-09-08 21:41:08 +0900

    敵AI簡易作成

--- Assets/Scripts/AiController.cs
+++ Assets/Scripts/AiController.cs
@@ -1,9 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AiController : MonoBehaviour
 {
+    /// <summary>
+    /// AIの移動先リスト
+    /// </summary>
+    [SerializeField]
+    private Transform[] _ai_pos_list = null;
+
+    /// <summary>
+    /// 次の移動先
+    /// </summary>
+    private Vector3 _next_position = new Vector3(0, 0, 0);
+
+    /// <summary>
+    /// AI移動機能
+    /// </summary>
+    private NavMeshAgent _agent;
+
     /// <summary>
     /// 色を変える時間
     /// </summary>
@@ -21,8 +38,32 @@ public class AiController : MonoBehaviour
     [SerializeField]
     private CharacterBase _character;
 
+    public void Start()
+    {
+        _Setup();
+    }
+
+    /// <summary>
+    /// セットアップ
+    /// </summary>
+    private void _Setup()
+    {
+        _agent = _character.GetComponent<NavMeshAgent>();
+        _agent.speed = _character.velocity;
+        _SetNextGoal();
+    }
+
     // Update is called once per frame
     void Update()
+    {
+        _GoalUpdate();
+        _ColorUpdate();
+    }
+
+    /// <summary>
+    /// 色チェンジチェック
+    /// </summary>
+    private void _ColorUpdate()
     {
         if (!_character.enableColorChange)
         {
@@ -36,6 +77,45 @@ public class AiController : MonoBehaviour
             _current_timer = 0;
             _character.ChangeColor(true);
         }
+    }
+
+    /// <summary>
+    /// ゴールチェック
+    /// </summary>
+    private void _GoalUpdate()
+    {
+        if(_agent == null)
+        {
+            return;
+        }
+
+        // ゴール近くまで来たら切り替え
+        if(_agent.remainingDistance < 0.5f)
+        {
+            _SetNextGoal();
+        }
+    }
+
+    /// <summary>
+    /// 次の位置情報を取得
+    /// </summary>
+    /// <returns></returns>
+    private void _SetNextGoal()
+    {
+        if (_agent == null)
+        {
+            return;
+        }
+        if (_ai_pos_list == null || _ai_pos_list.Length <= 0)
+        {
+            Debug.Log("AIの目的地が設定されていません");
+            return;
+        }
+
+        int num = Random.Range(0, _ai_pos_list.Length);
+        _next_position = _ai_pos_list[num].position;
+
+        _agent.destination = _next_position;
     }
 
 }
